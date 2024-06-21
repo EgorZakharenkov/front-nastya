7138c097669aff2ca48a43e44aafa61453994b02
Егор <egorboom2017@gmail.com>
2024-06-21 23:19:02 +0300

    fix

--- src/axios/axiosBase.ts
+++ src/axios/axiosBase.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "https://back-nastya.vercel.app",
 });
 
 instance.interceptors.request.use((config) => {
